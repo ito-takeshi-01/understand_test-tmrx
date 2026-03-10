bd4503b0f775942997831b547039b0e3e6e3fcea
JAPAN\3930492 <ito.takeshi@jp.panasonic.com>
2026-03-10 14:32:14 +0900

    update analyze.sh, clean.sh and generate-graph.sh

--- understand/clean.sh
+++ understand/clean.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -x
 
+# プロキシ設定用の環境変数（PROXY_HOST, PROXY_PORT）がJenkinsfileから渡されているか確認し、
+# 存在すればundコマンドをプロキシ設定付きのエイリアス（別名）で上書きする。
+# これにより、このスクリプト内で `und` と呼び出すと、プロキシ設定付きのコマンドが実行される。
+if [ -n "${PROXY_HOST:-}" ] && [ -n "${PROXY_PORT:-}" ]; then
+    alias und="und -proxyhost ${PROXY_HOST} -proxyport ${PROXY_PORT} -proxycreds ''"
+fi
+
 UND_DB_DIR="cgit-${GIT_COMMIT:=$(git show --summary --format=%H)}.und"
 
 und purge "${UND_DB_DIR}"
